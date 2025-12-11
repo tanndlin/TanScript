a61d18ab6be797f40588df678d89ea76216f578e
tanndlin <tanndlin@gmail.com>
2025-12-11 11:41:06 -0500

    Remove and save off docs

--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# React-ts-starter
-
-A starter repo for react in typescript with some tooling already configured
--- docs/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function App() {
-    return (
-        <div className="py-4 h-screen w-screen bg-primary">Hello World!</div>
-    );
-}
-
-export default App;
