ccef666f6443c9e6dbdba769518fae69e9d1802d
tanndlin <tanndlin@gmail.com>
2025-12-13 11:20:42 -0500

    Move RegisterHandler to own file

--- rs/src/compile.rs
+++ rs/src/compile.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::{collections::HashMap, fmt};
+use std::fmt;
 
 use crate::{
     ast::{
@@ -7,99 +7,8 @@ use crate::{
         Statement, StatementOrExpression, WhileLoop,
     },
     compile_scope::CompileScope,
+    register_handler::RegisterHandler,
 };
-
-pub struct RegisterHandler {
-    data: Vec<String>,
-    registers: HashMap<Register, bool>,
-    unique_id: u32,
-}
-
-impl RegisterHandler {
-    pub fn new() -> RegisterHandler {
-        RegisterHandler {
-            data: vec![],
-            registers: HashMap::from([
-                (Register::RAX, false),
-                (Register::RBX, false),
-                (Register::RCX, false),
-                (Register::RDX, false),
-                (Register::R8, false),
-                (Register::R9, false),
-                (Register::R10, false),
-                (Register::R11, false),
-                (Register::R12, false),
-                (Register::R13, false),
-                (Register::R14, false),
-                (Register::R15, false),
-            ]),
-            unique_id: 0,
-        }
-    }
-
-    pub fn lease_register(&mut self) -> Result<Register, String> {
-        let reg = self
-            .registers
-            .iter()
-            .skip_while(|(_, used)| **used)
-            .map(|(reg, _)| Ok(reg.clone()))
-            .nth(0)
-            .unwrap_or_else(|| Err("No registers available".to_string()))?;
-
-        self.registers.insert(reg.clone(), true);
-        Ok(reg)
-    }
-
-    pub fn release_register(&mut self, register: Register) {
-        match self.registers.get(&register) {
-            Some(_) => self.registers.insert(register, false),
-            None => panic!("Released unleasable register? {}", register),
-        };
-    }
-
-    fn add_data(&mut self, s: &str) -> String {
-        let name = format!("data_{}", self.data.len());
-        let format = format_data(&name, s);
-        self.data.push(format);
-
-        name
-    }
-
-    fn request_register(&mut self, register: &Register) -> Result<Register, String> {
-        match self.registers.get(register) {
-            None => Err(format!("Register {register} not leasable")),
-            Some(used) => match used {
-                true => Err(format!("Register {register} not available")),
-                false => {
-                    self.registers.insert(register.clone(), true);
-                    Ok(register.clone())
-                }
-            },
-        }
-    }
-
-    pub fn get_unique_id(&mut self) -> u32 {
-        let id = self.unique_id;
-        self.unique_id += 1;
-        id
-    }
-}
-
-fn format_data(name: &String, s: &str) -> String {
-    let mut pieces: Vec<&str> = s.split("\\n").collect();
-    if s.ends_with("\\n") {
-        pieces.pop();
-    }
-
-    let mut newlines = pieces.join("\", 10, \"");
-    if s.ends_with("\\n") {
-        newlines.push_str("\", 10");
-    } else {
-        newlines.push('"');
-    }
-
-    format!("{name} db \"{newlines}, 0")
-}
 
 #[allow(clippy::upper_case_acronyms)]
 #[derive(Hash, PartialEq, Eq, Clone)]
