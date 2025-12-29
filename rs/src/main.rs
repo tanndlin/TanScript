5135e436b0f69ec6ede945292ba4fa44306d36be
tanndlin <tanndlin@gmail.com>
2025-12-29 14:45:47 -0500

    Take file as arg

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,6 +1,4 @@
-use std::{fs, path::Path};
-
-use glob::glob;
+use std::{env::args, fs, path::PathBuf};
 
 mod ast;
 mod compile;
@@ -14,16 +12,18 @@ mod types;
 use crate::parser::parse;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let dir = std::env::current_dir()?;
-
-    let paths = glob(&format!("{}/**/*.tan", dir.display()))?;
-    let file = paths.into_iter().next().ok_or("No file found")??;
-    let file_as_string = fs::read_to_string(file)?;
+    let file: PathBuf = args()
+        .nth(1)
+        .expect("Expected .tan file as positional arg")
+        .into();
+    let file_as_string = fs::read_to_string(&file)?;
 
     let ast = parse(&file_as_string)?;
     println!("{ast}");
     let code = ast.compile()?;
-    fs::write(Path::new("script.asm"), code)?;
+
+    let asm_path = file.with_extension("asm");
+    fs::write(asm_path, code)?;
 
     Ok(())
 }
